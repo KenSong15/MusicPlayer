20411d3990b21a38af7aec5a5cd4b6d6bd167744
Ken <kensong96@gmail.com>
2019-06-30 17:54:40 -0400

    115D

--- app/src/main/java/com/example/z52song/musicbox/MainActivity.java
+++ app/src/main/java/com/example/z52song/musicbox/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.z52song.musicbox;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button prevButton;
     private Button playButton;
     private Button nextButton;
+    private Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +44,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int currentPos = mediaPlayer.getCurrentPosition();
                 int durationInt = mediaPlayer.getDuration();
 
-                leftTime.setText(playTime.format(new Date(currentPos)));
-                righTime.setText(playTime.format(new Date(durationInt - currentPos)));
+                leftTime.setText(String.valueOf(playTime.format(new Date(currentPos))));
+                righTime.setText(String.valueOf(playTime.format(new Date(durationInt - currentPos))));
             }
 
             @Override
@@ -62,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void setUpUI(){
 
         mediaPlayer = new MediaPlayer();
-        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.m1to20);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.mytwenty);
 
         artistImage = findViewById(R.id.topOval);
         leftTime = findViewById(R.id.leftTime);
@@ -75,16 +77,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         prevButton.setOnClickListener(this);
         playButton.setOnClickListener(this);
         nextButton.setOnClickListener(this);
+        righTime.setText(String.valueOf(new java.text.SimpleDateFormat("mm:ss").format(new Date(mediaPlayer.getDuration()))));
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.prevButton:
-                //TODO prev
+                backMusic();
                 break;
 
             case R.id.playButtion:
+
+                Log.d("duration", Integer.toString(mediaPlayer.getDuration()));
 
                 if(mediaPlayer.isPlaying()){
                     pauseMusic();
@@ -94,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.nextButton:
-                //TODO next
+                nextMusic();
                 break;
 
         }
@@ -111,7 +116,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(mediaPlayer != null) {
             mediaPlayer.start();
             playButton.setBackgroundResource(android.R.drawable.ic_media_pause);
+
+            updateThread();
         }
+    }
+
+    public void backMusic(){
+        if(mediaPlayer != null){
+            mediaPlayer.seekTo(0);
+            playMusic();
+        }
+    }
+
+    public void nextMusic(){
+        if(mediaPlayer != null){
+            mediaPlayer.seekTo(mediaPlayer.getDuration());
+            playButton.setBackgroundResource(android.R.drawable.ic_media_play);
+        }
+    }
+
+    //TODO: this could be the solution of the negative time
+    public void setTimeShow(int dur, TextView tv){
+        int mins = dur / 1000 / 60;
+        int secs = dur / 1000 % 60;
+        tv.setText(mins + ":" + secs);
+    }
+
+    public void updateThread(){
+        thread = new Thread(){
+
+            @Override
+            public void run() {
+                try{
+                    while(mediaPlayer != null && mediaPlayer.isPlaying()){
+
+                        Thread.sleep(50);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //update the seekbar
+                                int newPosition = mediaPlayer.getCurrentPosition();
+                                int newMax = mediaPlayer.getDuration();
+                                seekBar.setProgress(newPosition);
+                                seekBar.setMax(newMax);
+
+                                //update the text
+                                leftTime.setText(String.valueOf(new java.text.SimpleDateFormat("mm:ss")
+                                .format(new Date(mediaPlayer.getCurrentPosition()))));
+
+                                righTime.setText(String.valueOf(new java.text.SimpleDateFormat("mm:ss")
+                                        .format(new Date(mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition()))));
+
+                            }
+                        });
+                    }
+                } catch(Exception e){
+                    Log.e("seekbar thread", e.toString());
+                }
+            }
+        };
+        thread.start();
     }
 }
 
